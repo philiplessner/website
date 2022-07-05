bed21b8310f3ce19b7ed7da714f9eb4daaf33090
philiplessner <philiplessner@gmail.com>
2022-07-05 20:42:51 +0000

    Initial Commit

---
+++ flask_app.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template
+import datetime
+
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello():
+    now = datetime.datetime.now()
+    timeString = now.strftime("%Y-%m-%d %H:%M")
+    templateData = {
+            'title': 'Home',
+            'time': timeString
+            }
+    return render_template('index.html', **templateData)
+
+
+@app.route("/aboutme")
+def about_me():
+    templateData = {
+            'title': 'About'
+            }
+    return render_template('about_me.html', **templateData)
+
+
+@app.route("/photos/ecuador")
+def photos():
+    templateData = {
+            'title': 'Ecuador'
+            }
+    return render_template('photos_ecuador.html', **templateData)
