2956b33df3c0904c44ff2ab5efc17e8ebb419966
Philip Lessner <philiplessner@gmail.com>
2024-11-17 10:40:46 -0500

    Changed photos_ecuador to photos_template for use on multiple pages.  Added Brazil to Navbar. Created Brazil view and started to add photos.

--- flask_app.py
+++ flask_app.py
@@ -45,7 +45,27 @@ def photos_ecuador():
             ("https://lh3.googleusercontent.com/PJLSFsiOp63pwa5pIygGF76-vdiKGO0I_1rR8auYT0Pm5h287p4DSXPi8Fq9FqI__bjJ4Obwz0LHbTUbcjwtqqv-ezDIFK1pO1wgEUahIc9O4M9L3xQ_Clm2KNzVUDuzrnBL8p5viw=w2400", "Violet-Purple Coronet"),
             ("https://lh3.googleusercontent.com/4EQHzHkciQ5VN_U7-rBV0yGrburaafmjbytH46wbpveOvsRsrpP6SxHhrXLvXRzxtSh3ch0zPwA9H2G45wDV87F3f4pRbT03yJ4JUwnWVn9sD5cT0OKGqQhUg1fsO4VZFlYnW7apvQ=w2400", "Violet-Tailed Sylph")]}
 
-    return render_template('photos_ecuador.html', **templateData)
+    return render_template('photos_template.html', **templateData)
+
+
+@app.route("/photos/brazil")
+def photos_brazil():
+    templateData = {
+            "title": "Brazil",
+            "row1":  [("https://lh3.googleusercontent.com/pw/AP1GczNbh3-cbh9zwTBLjRC37KnWol4qL7jWnr5zmXi7AfNCgNvncbKMHeYGMx2JVu2xWpU-Ktzxs3jS4-pxKSkUYrupW1dPGg1k7aKCaSSk0L41ui57rZjBWl3o5n2DAmKKRgfjfvooaIgJ_0Sq---hSFEi=w1402-h933-s-no?authuser=0", "Male and Female Brazillian Tanager"),
+            ("https://lh3.googleusercontent.com/pw/AP1GczOOidVAU7_r3prZYmTzo2yyJBaedSL7n8xDLvobduNxa4wLGPzHvUaT8jRkfh4AnGJQO5omtbwFAEvasT6nX8XguhMCOuSqq1Vi02d12txWnusScM3gEKKJgKvbVa5j4-YrLh9sOeSGIUtLgcb5_agk=w1402-h933-s-no?authuser=0", "Versicolored Emerald"),
+            ("https://lh3.googleusercontent.com/pw/AP1GczOfZREyvb0K7z1qOTjWI_Vm9AyqlRF2Sym_6udZBJSoNl4hbeVxhFlDr9l947uTwJRzgVgH6_0jrCka0DnH_RCK-qm2QigLd_oCmaPrOudiNt7xcAhY0PXidUiWYMpPm_K9roW8HuTBdUEDoKqS33Qz=w1402-h933-s-no?authuser=0", "Brassy-Breasted Tanager")],
+            "row2":  [("https://lh3.googleusercontent.com/INJL-f9jw62okmBl4G7xMZypGPgpa9Vnmcf2OQjFJ1bJZGtetzBd-RpRx6xACJ2sHBrZLpKLasGtU4JsACkwa30w_R1MxUmw1C0rbusXcPwegLkP0-uHO6c4RHld8i82aZHYAE0seQ=w2400", "Male Ecuadorian Hillstar"),
+            ("https://lh3.googleusercontent.com/0DP0IeEReoODLYxg94F5cz9-N0ruXTthO8b0JEUW5pZg93Epw8lnFL_ZOSlMmYiBS6PqV0u8AU-SnYUmeVwYEGfPei54CLsgmqaPpwA_m5z_pGAHaZgDv700efSlCbf3oL4Bayd2cA=w2400", "Crowned Woodnymph"),
+            ("https://lh3.googleusercontent.com/QgUvXkcvBB-QnCROv8VabSQ0h30P2eLTVc7o4rnVUN_L5yI8W7-q54okoidsTcVfqx-9Zs9jXruSo8AKUhPN4OQF396gDneT6WuiLfRQRMXTB6XZ3Qpb47nL1kRkQI4ytdxy7lSY9Q=w2400", "White-Necked Jacobin")],
+            "row3":  [("https://lh3.googleusercontent.com/TO8Rqj4XrmnM_fIos57yghQpX-7xl8m9NBVA90Roex9XXJQ8m4BYZUT7b2dl9c4yXPfYE50Fp_9luoj1a235USEe50bN70OCd-KCNQmXNMBI_611SjTYR7flBZNY10A3cOT-mFwYCw=w2400", "Collared Aracari"),
+            ("https://lh3.googleusercontent.com/SI5bDdOoi-vbCIwnlGboq3ztA5uP9ysmOUI5tReJXefWXDu28QEIkCAbq4ZJLPE-FRcZcXqjxphRbAG6lKkyc032BsSY0ANzjY5gC859ZWUTpfcTV6LoeIBqlrDJqkOHpO8Cj2YPrA=w2400", "Andean Cock of the Rock"),
+            ("https://lh3.googleusercontent.com/eHNXVNRGNcSmoyYbsR23-_NFSc33Ytz9WerY-nVL6mb84-RPVjbJemtpaNah7d-6yRoAc5T1tkd2qne6aAPmhx-vdGq7FtqEIFuTCGpTcCyljtmdpuqK7pDPjz8AFybkmuEcjffUvA=w2400", "Male Masked Trogon")],
+            "row4":  [("https://lh3.googleusercontent.com/g85WMHn88SJs6I_veBFL7MmlQqSH-j3F10kXbtmiUbvmH31_5fqXOcQ9DMBRqpA-x9cr_E2_33tSeJjr2gy22AORYLOngcuUZL6IH3GvzIn-AOGFxdaRvFry9h0cf-COQ928zchTGw=w2400", "Buff-Winged Starfrontlet"),
+            ("https://lh3.googleusercontent.com/PJLSFsiOp63pwa5pIygGF76-vdiKGO0I_1rR8auYT0Pm5h287p4DSXPi8Fq9FqI__bjJ4Obwz0LHbTUbcjwtqqv-ezDIFK1pO1wgEUahIc9O4M9L3xQ_Clm2KNzVUDuzrnBL8p5viw=w2400", "Violet-Purple Coronet"),
+            ("https://lh3.googleusercontent.com/4EQHzHkciQ5VN_U7-rBV0yGrburaafmjbytH46wbpveOvsRsrpP6SxHhrXLvXRzxtSh3ch0zPwA9H2G45wDV87F3f4pRbT03yJ4JUwnWVn9sD5cT0OKGqQhUg1fsO4VZFlYnW7apvQ=w2400", "Violet-Tailed Sylph")]}
+
+    return render_template('photos_template.html', **templateData)
 
 
 if __name__ == "__main__":
