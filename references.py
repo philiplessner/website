2a3eeb2b3a98aef70760d622f95753b2a45da7ff
Philip Lessner <philiplessner@gmail.com>
2024-11-27 13:46:29 -0500

    Updated references. Changed copyright date to 2004. Wrote intro to about me and tweaked intro on home page.

--- references.py
+++ references.py
@@ -1,4 +1,5 @@
 import csv
+from collections import namedtuple
 from typing import List, Dict
 from pprint import pprint
 
@@ -6,20 +7,21 @@ from pprint import pprint
 def csv2list(csvfile: str) -> Dict:
     rows = list()
     references = dict()
+    Papers = namedtuple('Papers', ['authors', 'title', 'reference', 'date', 'link'])
     with open(csvfile, newline='') as f:
         reader = csv.reader(f, delimiter=',')
         for i, row in enumerate(reader):
             if (i != 0):
-                rows.append(tuple(row))
+                rows.append(Papers(*row))
     references.update({"references":rows})
     return references
 
 def print_references(references) -> None:
     for reference in references:
-        print(reference)
+        print(reference.authors)
 
 
 if __name__ == "__main__":
     references = csv2list("about_me.csv")
-#       pprint(references)
+#    pprint(references)
     print_references(**references)
