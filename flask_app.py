4a1dd644871eacea49f6abda41394aa4ad6d4423
Philip Lessner <philiplessner@gmail.com>
2024-12-21 17:50:44 -0500

    Added migrations from flask-migrate and created initial stamp

--- flask_app.py
+++ flask_app.py
@@ -1,4 +1,5 @@
 import os
+from flask_migrate import Migrate
 from app import create_app, db
 
 # We need to get this path to find the file. It will be different on the development and production server.
@@ -6,8 +7,9 @@ path2this_directory = os.path.abspath(os.path.dirname(__file__))
 app = create_app()
 app.config["SQLALCHEMY_DATABASE_URI"] = "".join(["sqlite:///", path2this_directory, "/app/db/website.db"])
 db.init_app(app)
-with app.app_context():
-    db.create_all()
+migrate = Migrate(app, db)
+#with app.app_context():
+#    db.create_all()
 
 
 if __name__ == "__main__":
