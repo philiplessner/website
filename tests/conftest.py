a63dfb9590b781748408a164e0179c4231c3dd63
Philip Lessner <philiplessner@gmail.com>
2024-12-26 16:18:57 -0500

    Removed old images.db file. Correct config for testing in fixture. Simplified path setup for databases.

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,11 +4,13 @@ from app import create_app, db
 
 @pytest.fixture(scope='module')
 def test_client():
-    os.environ['CONFIG_TYPE'] = 'config.TestingConfig'
     app = create_app()
     path2this_directory = os.path.abspath(os.path.dirname(__file__))
     path2parent_directory = os.path.abspath(os.path.join(path2this_directory, os.pardir))
-    app.config["SQLALCHEMY_DATABASE_URI"] = "".join(["sqlite:///", path2parent_directory,  "/app/db/website.db"])
+    app.config.update({
+        "TESTING": True,
+        "SQLALCHEMY_DATABASE_URI": f"sqlite:///{os.path.join(path2parent_directory, 'app/db/website.db')}"
+    })
     db.init_app(app)
     with app.app_context():
         db.create_all()
