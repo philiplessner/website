d4ad0ccdb17eb8c3d86b80a57eab0b4b01a76e86
Philip Lessner <philiplessner@gmail.com>
2026-06-26 17:17:52 -0400

    Updated Login page  to make flash alert Bootstrap style.

--- app/admin.py
+++ app/admin.py
@@ -1,5 +1,3 @@
-from re import A
-
 from flask import Blueprint, render_template, request, redirect, url_for, flash
 from flask_login import login_user, logout_user, login_required
 from werkzeug.security import generate_password_hash, check_password_hash
