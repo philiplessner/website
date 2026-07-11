4ee7238fa149c72f9aaef696ce6a5c7cd9a25085
Philip Lessner <philiplessner@gmail.com>
2026-07-11 15:01:12 -0400

    Remove URL imports from forms.py. Put html in blogedit.html in container.

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField, SelectField, TextAreaField, URLField
-from wtforms.validators import DataRequired, EqualTo, URL
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, SelectField, TextAreaField
+from wtforms.validators import DataRequired, EqualTo
 
 
 class LoginForm(FlaskForm):
