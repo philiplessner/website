f208e38dc480c397fc2be53a8ff980e5b0697523
Philip Lessner <philiplessner@gmail.com>
2026-08-29 11:07:57 -0400

    Added page for analytics (analytics in admin.py and analytics.html in templates.

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,7 @@
+import sqlite3
+from datetime import datetime, timedelta, timezone
+from pathlib import Path
+
 from flask import Blueprint, flash, redirect, render_template, request, url_for
 from flask_login import current_user, login_required, login_user, logout_user
 from werkzeug.security import check_password_hash, generate_password_hash
@@ -156,3 +160,38 @@ def blog_edit(blogid):
         else: # Data Error in form
             flash_form_errors(form)
             return render_template('blogedit.html', form=form, blogid=blogid)
+
+
+@admin.route('/analytics')
+def analytics():
+    if not current_user.is_authenticated:
+        return redirect(url_for('admin.login'))
+    country_query_humans = """
+    SELECT countryCode, COUNT(*) AS count
+    FROM logs
+    WHERE Agent_Type = 'H'
+    AND status_code <> 404
+    AND strftime('%Y-%m-%d %H:%M:%S', datetime) BETWEEN ? AND ?
+    GROUP BY countryCode
+    ORDER BY count DESC
+    """
+    grandparent = Path(__file__).resolve().parents[2]
+    path2db = Path(grandparent, 'logs/data.philiplessner.com/logs.db')
+    print(path2db)
+    conn = sqlite3.connect(path2db)
+    yesterday = (datetime.now(tz=timezone.utc) - timedelta(days=1)).strftime("%Y-%m-%d %H:%M:%S")
+    past = (datetime.now(tz=timezone.utc) - timedelta(days=31)).strftime("%Y-%m-%d %H:%M:%S")
+    country_counts_humans = conn.execute(country_query_humans, (past, yesterday)).fetchall()
+    conn.close()
+    countryCodes_humans = [countryCode for countryCode, _ in country_counts_humans]
+    counts_humans = [count for _, count in country_counts_humans]
+    total = sum(counts_humans)
+    percentages_humans = [count/total*100. for count in counts_humans]
+    templateData = {
+        'chart_data': {
+            'countryCodes': countryCodes_humans[:10],
+            'percentages': percentages_humans[:10],
+            'title': f'Percent Website Visits by Country for {past} to {yesterday}',
+        },
+    }
+    return render_template('analytics.html', **templateData)
