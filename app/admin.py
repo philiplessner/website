58481024ad2a02bc01770afc47cab88586be7e17
Philip Lessner <philiplessner@gmail.com>
2026-08-29 13:19:38 -0400

    Added Analytics to Navbar menu.

--- app/admin.py
+++ app/admin.py
@@ -1,7 +1,3 @@
-import sqlite3
-from datetime import datetime, timedelta, timezone
-from pathlib import Path
-
 from flask import Blueprint, flash, redirect, render_template, request, url_for
 from flask_login import current_user, login_required, login_user, logout_user
 from werkzeug.security import check_password_hash, generate_password_hash
